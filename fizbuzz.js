7263b245744c4f90efa3c1a4645c950cea392643
Wenzeltelger <100151419+Wenzeltelger@users.noreply.github.com>
2023-01-17 14:08:06 -0600

    Personal Portfolio 1.0

---
+++ fizbuzz.js
@@ -0,0 +1,15 @@
+let i = 10
+
+console.log(i)
+
+
+// for (i=1; i<=100; i++){
+//     let result = ''
+//     if(i % 3 === 0){
+//         result += 'Fizz'
+//     }
+//     if(i % 5 === 0){
+//         result += 'Buzz'
+//     }
+//     console.log(result || i)
+// }
